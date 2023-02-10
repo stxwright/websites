b277385ebd4573efa9edd4c3987bd0e5bfa8abb9
stx101 <sw.forward@gmail.com>
2023-02-10 19:09:27 +0000

    Added a stylesheet and some javascript

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  if (document.getElementById("demo").innerHTML === "Hello World") {
+    document.getElementById("demo").innerHTML = "Hello Paxton";
+  } else {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+}
